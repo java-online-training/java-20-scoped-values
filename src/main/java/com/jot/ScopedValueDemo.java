4d418aaadf9fbc963b9d81fcd5d95dcc2d54d3f6
superernie77 <superernie77@gmail.com>
2023-04-16 10:38:25 +0200

    initial commit

---
+++ src/main/java/com/jot/ScopedValueDemo.java
@@ -0,0 +1,12 @@
+package com.jot;
+
+public class ScopedValueDemo {
+
+	//private static ScopedValue<String> VALUE = 
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
