a3643a8a2c8e2031f8bda6f9219eabe6cbc11725
superernie77 <superernie77@gmail.com>
2023-04-18 11:58:19 +0200

    simple example

--- src/main/java/com/jot/ScopedValueDemo.java
+++ src/main/java/com/jot/ScopedValueDemo.java
@@ -2,12 +2,27 @@ package com.jot;
 
 import jdk.incubator.concurrent.ScopedValue;
 
+import java.util.List;
+
 public class ScopedValueDemo {
 
-	final static ScopedValue<String> STRINGSCOPE = ScopedValue.newInstance();
-	
-	public static void main(String[] args) {
+	final static ScopedValue<String> USERNAME = ScopedValue.newInstance();
 
+
+	public static void scopedValueInSimpleAction(List<String> usernames){
+
+		for (String username : usernames) {
+			ScopedValue.where(USERNAME, username).call( () -> {
+				String message = "Username is "+new USERNAME.get();
+				System.out.println(compound);
+				return message;
+			});
+		}
+
+
+	}
+	public static void main(String[] args) {
+		scopedValueInSimpleAction(List.of("Ernie", "Bert", "Jack"));
 	}
 
 }
