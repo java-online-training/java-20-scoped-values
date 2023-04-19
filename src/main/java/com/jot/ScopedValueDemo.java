79d45ffc5a56dd17f6b63b17f7e3caa3ad8ae71e
superernie77 <superernie77@gmail.com>
2023-04-19 17:21:03 +0200

    examples for usage

--- src/main/java/com/jot/ScopedValueDemo.java
+++ src/main/java/com/jot/ScopedValueDemo.java
@@ -2,27 +2,73 @@ package com.jot;
 
 import jdk.incubator.concurrent.ScopedValue;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.Callable;
 
 public class ScopedValueDemo {
 
-	final static ScopedValue<String> USERNAME = ScopedValue.newInstance();
+	public final static ScopedValue<String> USERNAME = ScopedValue.newInstance();
 
+    /* Simple example for the use of ScopedValues  */
 
-	public static void scopedValueInSimpleAction(List<String> usernames){
+    private static  Runnable printMessage = () -> {
+        String message = "Username is "+USERNAME.get();
+        System.out.println(message);
+    };
 
+	public static void scopedValueInSimpleAction(List<String> usernames) throws Exception {
 		for (String username : usernames) {
-			ScopedValue.where(USERNAME, username).call( () -> {
-				String message = "Username is "+new USERNAME.get();
-				System.out.println(compound);
-				return message;
-			});
+			ScopedValue.where(USERNAME, username).run(printMessage);
 		}
-
-
 	}
-	public static void main(String[] args) {
-		scopedValueInSimpleAction(List.of("Ernie", "Bert", "Jack"));
+
+    /* Example for isBound Method  */
+    private static  Runnable printMessageWithCheck = () -> {
+        if (USERNAME.isBound()){
+            String message = "Username is "+USERNAME.get();
+            System.out.println(message);
+        } else {
+            System.out.println("Username not bound");
+        }
+    };
+
+    public static void scopedValueWithCheck(List<String> usernames) throws Exception {
+        for (String username : usernames) {
+            ScopedValue.where(USERNAME, username).run(printMessageWithCheck);
+        }
+    }
+
+
+    /* Example for ScopedValues withCallable  */
+    private static  Callable<String> revertUsername = () ->  {
+        if (USERNAME.isBound() && USERNAME.get() != null) {
+            return new StringBuilder(USERNAME.get()).reverse().toString();
+        };
+        return "";
+    };
+
+
+    public static void reverseUsernames(List<String> usernames) throws Exception {
+        for (String username : usernames) {
+            System.out.println(ScopedValue.where(USERNAME, username).call(revertUsername));
+        }
+    }
+
+
+	public static void main(String[] args) throws Exception {
+
+        List<String> names = Arrays.asList("Ernie", "Bert", "Jack", null);
+
+		scopedValueInSimpleAction(names);
+
+        System.out.println("---------------------------------------");
+
+        scopedValueWithCheck(names);
+
+        System.out.println("---------------------------------------");
+
+        reverseUsernames(names);
 	}
 
 }
