b9d6db98d4a605c8e57418933483beb1f3cb099a
superernie77 <superernie77@gmail.com>
2023-04-22 13:31:51 +0200

    added VThread example

--- src/main/java/com/jot/ScopedValueDemo.java
+++ src/main/java/com/jot/ScopedValueDemo.java
@@ -1,10 +1,12 @@
 package com.jot;
 
 import jdk.incubator.concurrent.ScopedValue;
+import jdk.incubator.concurrent.StructuredTaskScope;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
 
 public class ScopedValueDemo {
 
@@ -13,35 +15,69 @@ public class ScopedValueDemo {
     /* Simple example for the use of ScopedValues  */
 
     private static  Runnable printMessage = () -> {
+        System.out.println(Thread.currentThread());
         String message = "Username is "+USERNAME.get();
         System.out.println(message);
+        System.out.println(Thread.currentThread());
     };
 
 	public static void scopedValueInSimpleAction(List<String> usernames) throws Exception {
+        System.out.println(Thread.currentThread());
 		for (String username : usernames) {
 			ScopedValue.where(USERNAME, username).run(printMessage);
 		}
 	}
 
     /* Example for isBound Method  */
-    private static  Runnable printMessageWithCheck = () -> {
-        if (USERNAME.isBound()){
-            String message = "Username is "+USERNAME.get();
-            System.out.println(message);
-        } else {
-            System.out.println("Username not bound");
+    private static class MyRunnable implements Runnable {
+        public void run() {
+            System.out.println(Thread.currentThread());
+            if (USERNAME.isBound()) {
+                String message = "Username is " + USERNAME.get();
+                System.out.println(message);
+            } else {
+                System.out.println("Username not bound");
+            }
         }
-    };
+    }
+
+    private static class VThreadStarter implements Runnable {
+        public void run() {
+            System.out.println(Thread.currentThread());
+            try (var scope = new StructuredTaskScope.ShutdownOnFailure()) {
+                Future<String> task1 =  scope.fork(revertUsername);
+                Future<String> task2 =scope.fork(revertUsername);
+                Future<String> task3 =scope.fork(revertUsername);
+                try {
+                    scope.join();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println(task1.resultNow());
+                System.out.println(task2.resultNow());
+                System.out.println(task3.resultNow());
+            }
+
+        }
+    }
+
 
     public static void scopedValueWithCheck(List<String> usernames) throws Exception {
         for (String username : usernames) {
-            ScopedValue.where(USERNAME, username).run(printMessageWithCheck);
+            ScopedValue.where(USERNAME, username).run(new MyRunnable());
+        }
+    }
+
+    public static void startVthreads(List<String> usernames) throws Exception {
+        for (String username : usernames) {
+            ScopedValue.where(USERNAME, username).run(new VThreadStarter());
         }
     }
 
 
     /* Example for ScopedValues withCallable  */
     private static  Callable<String> revertUsername = () ->  {
+        System.out.println(Thread.currentThread());
         if (USERNAME.isBound() && USERNAME.get() != null) {
             return new StringBuilder(USERNAME.get()).reverse().toString();
         };
@@ -50,25 +86,31 @@ public class ScopedValueDemo {
 
 
     public static void reverseUsernames(List<String> usernames) throws Exception {
+        System.out.println(Thread.currentThread());
         for (String username : usernames) {
-            System.out.println(ScopedValue.where(USERNAME, username).call(revertUsername));
+            System.out.println("Reversed Username is "+ScopedValue.where(USERNAME, username).call(revertUsername));
         }
     }
 
 
 	public static void main(String[] args) throws Exception {
 
-        List<String> names = Arrays.asList("Ernie", "Bert", "Jack", null);
+        List<String> names = Arrays.asList("Ernie", "Bert", "Jack");
 
-		scopedValueInSimpleAction(names);
+		//scopedValueInSimpleAction(names);
+
+        //System.out.println("---------------------------------------");
+
+        //new MyRunnable().run();
+        //scopedValueWithCheck(names);
+
+        //System.out.println("---------------------------------------");
+
+        //reverseUsernames(names);
 
         System.out.println("---------------------------------------");
 
-        scopedValueWithCheck(names);
-
-        System.out.println("---------------------------------------");
-
-        reverseUsernames(names);
+        startVthreads(names);
 	}
 
 }
